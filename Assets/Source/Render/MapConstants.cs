7147011308af37cf978e070d782165b82f0e339d
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-12-12 16:16:30 +0200

    Improved rendering of places where 2 terrain types meet. Loaded map is now a Resource that is loaded with Unity's Resources class.

--- Assets/Source/Render/MapConstants.cs
+++ Assets/Source/Render/MapConstants.cs
@@ -5,10 +5,27 @@ public class MapConstants {
 
     public const int WIDTH = 40;
     public const int HEIGHT = 30;
-    public static Dictionary<char, string> tileMap = new Dictionary<char, string>();
+
+    private static Dictionary<char, string> surfaceMap = new Dictionary<char, string>();
+    private static Dictionary<char, string> dungeonMap = new Dictionary<char, string>();
+
+    public static Dictionary<char, string> GetTileSet(TileSet tileSet) {
+        if (tileSet == TileSet.SURFACE) {
+            return surfaceMap;
+        } else if (tileSet == TileSet.DUNGEON) {
+            return dungeonMap;
+        } else {
+            return surfaceMap;
+        }
+    }
+
+    public enum TileSet { SURFACE, DUNGEON }
 
     static MapConstants() {
-        tileMap.Add('.', "dirt");
-        tileMap.Add(',', "grass");
+        surfaceMap.Add('.', "dirt");
+        surfaceMap.Add(',', "grass");
+
+        dungeonMap.Add('.', "dungeondirt");
+        dungeonMap.Add(',', "dungeon");
     }
 }
--- Assets/Source/Render/MapLoader.cs
+++ Assets/Source/Render/MapLoader.cs
@@ -11,6 +11,113 @@ public class MapLoader : MonoBehaviour {
     private string[] mapTileNames;
     private int[] surroundingPositionValues = { 1, 2, 4, 8, 0, 16, 32, 64, 128 };
     private static Dictionary<int, string> positionIdsToNames;
+    private Dictionary<char, string> tileSet;
+    private string primary;
+    private string secondary;
+
+	void Start () {
+        tileSet = MapConstants.GetTileSet(MapConstants.TileSet.SURFACE);
+        primary = tileSet['.'];
+        secondary = tileSet[','];
+
+        mapTileNames = loadMap();
+
+        prettifyTerrainEdges();
+
+        renderMap();
+    }
+	
+	void Update () {
+	}
+
+    private string[] loadMap() {
+        Debug.Log(System.Environment.Version);
+        string[] result = new string[MapConstants.WIDTH * MapConstants.HEIGHT];
+
+        TextAsset textFile = Resources.Load<TextAsset>(mapFile);
+        char[] data = System.Text.Encoding.ASCII.GetString(textFile.bytes).ToCharArray();
+        data = filterData(data);
+
+        for (int y = 0; y < MapConstants.HEIGHT; y++) {
+            for (int x = 0; x < MapConstants.WIDTH; x++) {
+                int idx = calcMapIndex(x, y);
+
+                char mapChar = data[idx];
+                string tileName;
+                tileSet.TryGetValue(mapChar, out tileName);
+                if (tileName == null) {
+                    tileName = primary;
+                }
+                result[idx] = tileName;
+            }
+        }
+
+        return result;
+    }
+    
+    private char[] filterData(char[] data) {
+        char[] dataFiltered = new char[data.Length];
+
+        int insertPos = 0;
+        for (int i = 0; i < data.Length; i++) {
+            if (tileSet.ContainsKey(data[i])) {
+                dataFiltered[insertPos++] = data[i];
+            }
+        }
+
+        return dataFiltered;
+    }
+
+    private void prettifyTerrainEdges() {
+        for (int y = 0; y < MapConstants.HEIGHT; y++) {
+            for (int x = 0; x < MapConstants.WIDTH; x++) {
+                string tile = mapTileNames[calcMapIndex(x, y)];
+                if (tile.Equals(secondary)) {
+                    int positionId = 0;
+
+                    for (int yy = -1; yy <= 1; yy++) {
+                        for (int xx = -1; xx <= 1; xx++) {
+                            if (yy == 0 && xx == 0) continue;
+
+                            int idx = calcMapIndex(x + xx, y + yy);
+                            string tileName = (idx < 0 || idx >= mapTileNames.Length) ? secondary : mapTileNames[idx];
+                            string[] tileNameParts = tileName.Split('-');
+                            if (tileNameParts[0].Equals(primary)) {
+                                int positionIdx = (yy + 1) * 3 + (xx + 1);
+                                positionId += surroundingPositionValues[positionIdx];
+                            }
+                        }
+                    }
+
+                    if (positionId > 0) {
+                        string extension;
+                        positionIdsToNames.TryGetValue(positionId, out extension);
+
+                        if (extension != null && !extension.Equals("")) {
+                            mapTileNames[calcMapIndex(x, y)] = secondary + "-" + primary + extension;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void renderMap() {
+        boardHolder = new GameObject().transform;
+
+        for (int y = 0; y < MapConstants.HEIGHT; y++) {
+            for (int x = 0; x < MapConstants.WIDTH; x++) {
+                GameObject instance = Instantiate(floor, new Vector3(x, MapConstants.HEIGHT - y - 1, 0f), Quaternion.identity) as GameObject;
+                SpriteRenderer renderer = instance.GetComponent<SpriteRenderer>();
+                renderer.sprite = SpriteRepository.GetSpriteByName(mapTileNames[calcMapIndex(x, y)]);
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    private int calcMapIndex(int x, int y) {
+        return y * MapConstants.WIDTH + x;
+    }
 
     static MapLoader() {
         positionIdsToNames = new Dictionary<int, string>();
@@ -24,6 +131,9 @@ public class MapLoader : MonoBehaviour {
         positionIdsToNames.Add(43, "-nw");
         positionIdsToNames.Add(47, "-nw");
 
+        positionIdsToNames.Add(2, "-n");
+        positionIdsToNames.Add(3, "-n");
+        positionIdsToNames.Add(6, "-n");
         positionIdsToNames.Add(7, "-n");
 
         positionIdsToNames.Add(22, "-ne");
@@ -31,6 +141,9 @@ public class MapLoader : MonoBehaviour {
         positionIdsToNames.Add(150, "-ne");
         positionIdsToNames.Add(151, "-ne");
 
+        positionIdsToNames.Add(16, "-e");
+        positionIdsToNames.Add(20, "-e");
+        positionIdsToNames.Add(144, "-e");
         positionIdsToNames.Add(148, "-e");
 
         positionIdsToNames.Add(208, "-se");
@@ -38,6 +151,9 @@ public class MapLoader : MonoBehaviour {
         positionIdsToNames.Add(240, "-se");
         positionIdsToNames.Add(244, "-se");
 
+        positionIdsToNames.Add(64, "-s");
+        positionIdsToNames.Add(96, "-s");
+        positionIdsToNames.Add(192, "-s");
         positionIdsToNames.Add(224, "-s");
 
         positionIdsToNames.Add(104, "-sw");
@@ -45,6 +161,9 @@ public class MapLoader : MonoBehaviour {
         positionIdsToNames.Add(232, "-sw");
         positionIdsToNames.Add(233, "-sw");
 
+        positionIdsToNames.Add(8, "-w");
+        positionIdsToNames.Add(9, "-w");
+        positionIdsToNames.Add(40, "-w");
         positionIdsToNames.Add(41, "-w");
 
         positionIdsToNames.Add(31, "-top");
@@ -71,103 +190,5 @@ public class MapLoader : MonoBehaviour {
         positionIdsToNames.Add(231, "-horizontal");
 
         positionIdsToNames.Add(255, "-alone");
-    }
-
-	void Start () {
-        mapTileNames = loadMap();
-
-        prettifyTerrainEdges();
-
-        renderMap();
-    }
-	
-	void Update () {
-	}
-
-    private string[] loadMap() {
-        Debug.Log(System.Environment.Version);
-        string[] result = new string[MapConstants.WIDTH * MapConstants.HEIGHT];
-        char[] data = System.Text.Encoding.ASCII.GetString(System.IO.File.ReadAllBytes(mapFile)).ToCharArray();
-        data = filterData(data);
-
-        for (int y = 0; y < MapConstants.HEIGHT; y++) {
-            for (int x = 0; x < MapConstants.WIDTH; x++) {
-                int idx = calcMapIndex(x, y);
-
-                char mapChar = data[idx];
-                string tileName;
-                MapConstants.tileMap.TryGetValue(mapChar, out tileName);
-                if (tileName == null) {
-                    tileName = "dirt";
-                }
-                result[idx] = tileName;
-            }
-        }
-
-        return result;
-    }
-    
-    private char[] filterData(char[] data) {
-        char[] dataFiltered = new char[data.Length];
-
-        int insertPos = 0;
-        for (int i = 0; i < data.Length; i++) {
-            if (MapConstants.tileMap.ContainsKey(data[i])) {
-                dataFiltered[insertPos++] = data[i];
-            }
-        }
-
-        return dataFiltered;
-    }
-
-    private void prettifyTerrainEdges() {
-        for (int y = 0; y < MapConstants.HEIGHT; y++) {
-            for (int x = 0; x < MapConstants.WIDTH; x++) {
-                string tile = mapTileNames[calcMapIndex(x, y)];
-                if (tile.Equals("grass")) {
-                    int positionId = 0;
-
-                    for (int yy = -1; yy <= 1; yy++) {
-                        for (int xx = -1; xx <= 1; xx++) {
-                            if (yy == 0 && xx == 0) continue;
-
-                            int idx = calcMapIndex(x + xx, y + yy);
-                            string tileName = (idx < 0 || idx >= mapTileNames.Length) ? "grass" : mapTileNames[idx];
-                            string[] tileNameParts = tileName.Split('-');
-                            if (tileNameParts[0].Equals("dirt")) {
-                                int positionIdx = (yy + 1) * 3 + (xx + 1);
-                                positionId += surroundingPositionValues[positionIdx];
-                            }
-                        }
-                    }
-
-                    if (positionId > 0) {
-                        string extension;
-                        positionIdsToNames.TryGetValue(positionId, out extension);
-
-                        if (extension != null && !extension.Equals("")) {
-                            mapTileNames[calcMapIndex(x, y)] = "grass-dirt" + extension;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void renderMap() {
-        boardHolder = new GameObject().transform;
-
-        for (int y = 0; y < MapConstants.HEIGHT; y++) {
-            for (int x = 0; x < MapConstants.WIDTH; x++) {
-                GameObject instance = Instantiate(floor, new Vector3(x, MapConstants.HEIGHT - y - 1, 0f), Quaternion.identity) as GameObject;
-                SpriteRenderer renderer = instance.GetComponent<SpriteRenderer>();
-                renderer.sprite = SpriteRepository.GetSpriteByName(mapTileNames[calcMapIndex(x, y)]);
-                instance.transform.SetParent(boardHolder);
-            }
-        }
-    }
-
-    private int calcMapIndex(int x, int y) {
-        return y * MapConstants.WIDTH + x;
     }
 }
